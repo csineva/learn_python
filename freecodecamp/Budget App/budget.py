e2a45cd8ebb24ff697a0807fa49c996962b4982d
benedek_istvan <csineva@gmail.com>
2023-04-28 13:01:43 +0200

    budget.py modified

--- freecodecamp/Budget App/budget.py
+++ freecodecamp/Budget App/budget.py
@@ -1,8 +1,23 @@
 class Category:
-    pass
+    def __init__(self, name):
+        self.name = name
+        self.ledger = []
 
+    def deposit(self, amount, description=""):
+        self.ledger.append({"amount": amount, "description": description})
 
-
+    def withdraw(self, amount, description=""):
+        funds = 0
+        for item in self.ledger:
+            funds += amount
+        return funds
 
 def create_spend_chart(categories):
     pass
+
+food = Category("Food")
+food.deposit(100, "some money")
+print(food.ledger)
+food.deposit(600, "some more money")
+print(food.ledger)
+print(food.withdraw(1))
