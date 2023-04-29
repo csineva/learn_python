bb10c8e018d5e8ad344b4c9cb52a4465e6a4e6a4
benedek_istvan <csineva@gmail.com>
2023-04-29 21:00:02 +0200

    Probability calculator added

---
+++ freecodecamp/Probability Calculator/prob_calculator.py
@@ -0,0 +1,10 @@
+# https://www.freecodecamp.org/learn/scientific-computing-with-python/scientific-computing-with-python-projects/probability-calculator
+
+# Consider using the modules imported above.
+
+class Hat:
+    pass
+
+
+def experiment(hat, expected_balls, num_balls_drawn, num_experiments):
+    pass
